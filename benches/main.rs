fcd34f9c6ad786e13d42cbace74521598b0c9331
Sebastian Jastrzebski <sebby2k@gmail.com>
2022-08-07 18:38:40 -0400

    project: code format

--- benches/main.rs
+++ benches/main.rs
@@ -1,9 +1,13 @@
 #[macro_use]
 extern crate criterion;
 
-mod sid_bench;
 mod sampler_bench;
+mod sid_bench;
 
-criterion_group!(benches, sid_bench::bench_sid, sampler_bench::bench_compute_convolution_fir);
+criterion_group!(
+    benches,
+    sid_bench::bench_sid,
+    sampler_bench::bench_compute_convolution_fir
+);
 
 criterion_main!(benches);
